68e01ff1d4693314d69ce81b8fb4736437e38a3f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-06 14:28:36 +0000

    Tue, Feb 6, 2024, 7:58 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 ans=input("What is the answer to the Great Question of Life,the Universe ,and Everything?")
-ans=ans.strip
+ans=ans.strip()
 if ans=="42" or ans=="forty-two" or ans=="forty two":
     print("Yes")
 else:
