eb6516883567436bc88aa67cc5bf0f1a0ca4aca9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-10 14:10:08 +0000

    Sat, Feb 10, 2024, 7:40 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,3 +1,22 @@
+def main():
+    time=input("What time is it?")
+    convert(time)
+
+def convert(time):
+    hours,minutes=time.split(":")
+    hours=int(hours)
+    minutes=int(minutes)
+    if hours==7 or hours==8 and minutes<=60:
+        print("breakfast time")
+        if hours==7 and minutes==30:
+            print("7.5")
+    elif hours==12 or hours==13  and minutes<=60:
+        print("lunch time")
+    elif hours==18 or hours==19 and minutes<=60:
+        print("dinner time")
+
+if __name__ == "__main__":
+    main()
 
 
 
