99031b90d11a1906224cad610047c8e71cfd0262
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-10 12:24:08 +0000

    Sat, Feb 10, 2024, 5:54 PM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,7 +1,20 @@
 filetype=input("Enter the type of the file:").strip()
-match filetype:
-    case filetype.endswith("jpg"):
-          print("application/jpg")
-    case filetype.enswith("gif"):
-          print("application/gif")
+if filetype.endswith("jpg"):
+     print("application/jpg")
+elif filetype.endswith("gif"):
+     print("application/gif")
+elif filetype.endswith("jpeg"):
+     print("application/jpeg")
+elif filetype.endswith("png"):
+     print("application/png")
+elif filetype.endswith("pdf"):
+     print("application/pdf")
+elif filetype.endswith("txt"):
+     print("application/txt")
+elif filetype.endswith("zip"):
+     print("application/zip")
+else :
+     pritnf("application/octet-stream")
+
+
 
