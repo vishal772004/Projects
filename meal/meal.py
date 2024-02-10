dc8c4effed3a91a2335dc4a594c3142b02e48100
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-10 13:19:15 +0000

    Sat, Feb 10, 2024, 6:49 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -7,11 +7,11 @@ def convert(time):
     hours,minutes=time.split(":")
     hours=int(hours)
     minutes=int(minutes)
-    if hours==(7 or 8) and minutes<=60:
+    if hours==7 and hours==8 and minutes<=60:
         print("breakfast time")
-    elif hours==(12 or 13) and minutes<=60:
+    elif hours==12 and hours==13  and minutes<=60:
         print("lunch time")
-    elif hours==(18 or 19) and minutes<=60:
+    elif hours==18 and hours==19 and minutes<=60:
         print("dinner time")
 
 
