0baa580e24e823121b217680b07da41c1d236a58
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-10 14:18:06 +0000

    Sat, Feb 10, 2024, 7:48 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -6,8 +6,6 @@ def main():
     minutes=int(minutes)
     if hours==7 or hours==8 and minutes<=60:
         print("breakfast time")
-        if hours==7 and minutes==30:
-            print("7.5")
     elif hours==12 or hours==13  and minutes<=60:
         print("lunch time")
     elif hours==18 or hours==19 and minutes<=60:
@@ -15,7 +13,9 @@ def main():
 
 def convert(time):
     h,m=time.split(":")
-    
+    h=float(h)
+    m=float(m)
+    return h+m/60.0
 
 
 if __name__ == "__main__":
