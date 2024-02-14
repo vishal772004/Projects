22cc5f4cfdc855ab40069b6e96cd8955b934b88f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-14 15:09:24 +0000

    Wed, Feb 14, 2024, 8:39 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -2,8 +2,9 @@ print("Amount Due:50")
 amt=int(input("Insert Coin:"))
 amt1=50
 while amt>0:
-    if amt==5 or amt==10 or amt==25 and amt1>0:
-        print("Amount Due:",amt1-amt)
+    if amt==5 or amt==10 or amt==25:
+        if amt1>0:
+            print("Amount Due:",amt1-amt)
     else:
         print("Amount Due:50")
     if amt1<amt:
