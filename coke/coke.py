b222b5e3b24b3899e5d65f355e00af39656fedca
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-14 14:00:03 +0000

    Wed, Feb 14, 2024, 7:30 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -4,7 +4,8 @@ amt1=50
 while amt!=0:
      if amt==5 or amt==10 or amt==25:
          amt=amt1-amt
-         print("Amount Due:",amt)
+         if amt>0:
+             print("Amount Due:",amt)
      else:
          print("Amount Due:50")
      amt1=amt
