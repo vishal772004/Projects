c72652c93bf725725d30c9160eca770dc70a2296
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-14 13:40:43 +0000

    Wed, Feb 14, 2024, 7:10 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,7 +1,8 @@
 print("Amount Due:50")
 amt=int(input("Insert coin:"))
+amt1=50-amt
 while amt!=0:
      if amt==5 or amt==10 or amt==25:
-         amt=50-amt
+         amt1=50-amt
          print("Change owed:",amt)
      amt=int(input("Insert coin:"))
