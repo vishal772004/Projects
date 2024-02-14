6a85d4d81db8e86b7e6036808cee3b0eba72f3b6
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-14 14:28:55 +0000

    Wed, Feb 14, 2024, 7:58 PM +05:30

--- coke/coke.py
+++ coke/coke.py
@@ -1,23 +1,15 @@
 print("Amount Due:50")
 amt=int(input("Insert coin:"))
 amt1=50
-while amt!=0:
+while amt>=0:
 
      if amt==5 or amt==10 or amt==25:
-         if amt1>amt:
-            amt=amt1-amt
-            if amt>0:
-                print("Amount Due:",amt)
-            else:
-                print("Amount Due:50")
+         if amt<amt1:
+               amt=amt1-amt
+               print("Amount Due:",amt)
+         else:
+               print("Amount Due:50")
      amt1=amt
+     if amt
 
-     if amt>amt1:
-         print("Change Owed:",amt1)
-         break
-     elif amt1==0:
-         print("Change Owed:",amt1)
-         break
-     if amt1>=0:
-         amt=int(input("Insert coin:"))
 
