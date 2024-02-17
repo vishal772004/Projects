565bc5bb33963839031b7d95e8420e178914d1e9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 18:51:12 +0000

    Sun, Feb 18, 2024, 12:21 AM +05:30

---
+++ twttr/twttr.py
@@ -0,0 +1,9 @@
+phrase=input("Input:")
+l=[]
+for i in phrase:
+    l.append(i)
+vowels=["A","E","I","O","U","a","e","i","o","u"]
+for i in phrase:
+    for j in vowels:
+        phrase=phrase.replace(i,"")
+print("Output:",phrase)
