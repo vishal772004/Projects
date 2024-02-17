953d7882eb91f56550e3c13548801d530e5b8d6f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 18:22:01 +0000

    Sat, Feb 17, 2024, 11:52 PM +05:30

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,11 +1,2 @@
-phrase=input("Input:").split()
-l=[]
-for j in phrase:
-    l.append(j)
-    print(l)
-#vowels=["A","E","I","O","U","a","e","i","o","u"]
-#for i in l:
- #   for k in vowels:
-#        if i==k:
- #           l.replace(i," ")
-
+phrase=str(input("Input:")).split()
+print(phrase)
