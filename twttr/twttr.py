3ba6783c84de5d2d5b79c57d43fc8a7a046f369b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 18:05:53 +0000

    Sat, Feb 17, 2024, 11:35 PM +05:30

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,9 +1,7 @@
+phrase=list()
 phrase=input("Input:").split()
 for str in phrase:
-    str=str.upper()
-    if str=="A" or str=="E" or str=="I" or str=="O" or str=="U":
-        str=str.replace(str,"")
-        print(str)
+    print(str)
 print("Output:",phrase)
 
 
