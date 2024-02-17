6579200d17c78efcdfc5c639590e1a9356474733
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 18:59:32 +0000

    Sun, Feb 18, 2024, 12:29 AM +05:30

---
+++ plates/plates.py
@@ -0,0 +1,16 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    l=[]
+    for i in plate:
+        l.append(i)
+    
+
+
+main()
