3c8fec4e199dd0527279117df547ec1cce774202
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 19:16:21 +0000

    Sun, Feb 18, 2024, 12:46 AM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -13,10 +13,12 @@ def is_valid(s):
         l.append(i)
         if i.isdigit():
             if i==0 and j==0:
-                j=j+1
                 return False
+            j=j+1
     if plate[-1].isdigit():
         pass
+    else:
+        return False
 
 
     if plate[0].isalpha() or plate[1].isalpha():
