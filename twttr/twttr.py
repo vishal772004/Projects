1bc5ce882ad302666b82fd91e88c8200f49ffd8f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-17 18:02:53 +0000

    Sat, Feb 17, 2024, 11:32 PM +05:30

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,3 +1,9 @@
-phrase=input("Input:")
+phrase=input("Input:").split
 for str in phrase:
-    if 
+    str=str.upper()
+    if str=="A" or str=="E" or str=="I" or str=="O" or str=="U":
+        str=str.replace(str,"")
+        print(str)
+print("Output:",phrase)
+
+
