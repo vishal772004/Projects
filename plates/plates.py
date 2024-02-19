6aba169e42d6f1a324dd5ece4cb3c40505096eda
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-19 15:27:23 +0000

    Mon, Feb 19, 2024, 8:57 PM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -1,5 +1,5 @@
 def main():
-    plate= input("Plate: ")
+    plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
     else:
@@ -8,30 +8,9 @@ def main():
 
 def is_valid(s):
     l=[]
-    j=0
     for i in s:
         l.append(i)
-        if i.isdigit():
-            if i==0 and j==0:
-                return True
-            j=j+1
-        if s[-1].isdigit():
-             pass
-        else:
-             return False
+        
 
 
-    if s[0].isalpha() or s[1].isalpha():
-        pass
-    else:
-        return False
-    if 2>len(l)<=6:
-        pass
-    else:
-        return False
-    if s.isalnum():
-        pass
-    else:
-        return False
-    return True
 main()
