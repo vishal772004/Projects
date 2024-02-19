35a69d98acfd7709be078c021a5e73d449ac6bfc
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-19 15:48:43 +0000

    Mon, Feb 19, 2024, 9:18 PM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -13,7 +13,18 @@ def is_valid(s):
         l.append(i)
         if i>=0 and i<=9:
             j=j+1
-
+    if 2<=len(l)<=6:
+        pass
+    else:
+        return False
+    if j>0 and l[-1]>0 and l[-1]<9 :
+        pass
+    else :
+        return False
+    if s.isalnum():
+        pass
+    else :
+        return False
 
 
     print(l)
