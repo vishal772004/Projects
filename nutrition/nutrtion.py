59c17178d6d1bee9f171431039e7f46cb992fa56
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-20 13:14:53 +0000

    Tue, Feb 20, 2024, 6:44 PM +05:30

--- nutrition/nutrtion.py
+++ nutrition/nutrtion.py
@@ -1,3 +1,16 @@
 item=input("Item:")
 match item:
     case "Apple":
+        print("Calories:130")
+    case "Avocado":
+        print("Calories:50")
+    case "Banana":
+        print("Calories:110")
+    case "Cantalope":
+        print("Calories:50")
+    case "Grapefruit":
+        print("Calories:60")
+    case "Grapes":
+        print("Calories:90")
+    case "Honeydew Melon":
+        print("Calories:50")
