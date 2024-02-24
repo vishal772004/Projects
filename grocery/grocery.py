e46ccdfd8925e0839c2f5dfec755eb372b2c6210
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 19:56:33 +0000

    Sun, Feb 25, 2024, 1:26 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -10,14 +10,9 @@ d={}
 k=[]
 a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-"""for  i in l:
-    for j in l:
-        if i==j:
-            z=z+1
-            l.remove(j)
-        d[i]=z
-        z=1
-        """
+for k in l:
+    for m in l:
+        
 for i in a:
     for j in l:
         if j.startswith(i):
