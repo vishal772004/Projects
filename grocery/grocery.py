fcdeb5769220ddaabd19499cd50cd71b6c550638
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 20:33:43 +0000

    Sun, Feb 25, 2024, 2:03 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -7,7 +7,7 @@ while True:
         break
 z=0
 d={}
-s={}
+s=set()
 a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 x=len(l)
 for k in l:
@@ -24,4 +24,5 @@ for i in a:
         if j.startswith(i):
             print(d[j],j)
 print(d)
+print(s)
 
