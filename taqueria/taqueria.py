c9c0315becaf5fb3ccd773f233d628f65afae226
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 18:19:19 +0000

    Sat, Feb 24, 2024, 11:49 PM +05:30

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,13 +1,13 @@
 menu={
-    "Baja Taco": 4.25,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "baja taco": 4.25,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00
 }
 sum=0
 while True:
