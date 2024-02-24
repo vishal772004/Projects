4ab76e29049ce83166489bd4b95d2e559a590003
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 19:26:54 +0000

    Sun, Feb 25, 2024, 12:56 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,13 +6,16 @@ while True:
     except EOFError:
         break
 z=0
+d={}
 a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 for i in l:
     for j in l:
         if i==j:
             z=z+1
     for k in i:
-        if 
-
+        for l in a:
+            if k==a:
+                d[i]=len(a)
+print(d)
 
 
