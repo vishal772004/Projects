a4d11446d586bd8006f56cb7f7f194d789a181bd
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 19:12:57 +0000

    Sun, Feb 25, 2024, 12:42 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,3 +1,11 @@
+l=[]
 while True:
     try :
-        g=input()
+        g=input().upper()
+        l.append(g)
+    except EOFError:
+        break
+for i in l:
+    for j in l:
+        if l[i]<l[j]:
+            print(l[i])
