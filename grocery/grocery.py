fe46c8c945fe191a4b90a7af64f160243f585d72
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 20:04:15 +0000

    Sun, Feb 25, 2024, 1:34 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -12,11 +12,15 @@ a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","
 x=len(l)
 for k in l:
     for m in l:
-        
+        if k==m and z>1:
+            z=z+1
+            l.remove(m)
+        d[k]=z
+
 
 for i in a:
     for j in l:
         if j.startswith(i):
-            print(j)
-
+            print(d[j],j)
+print(d)
 
