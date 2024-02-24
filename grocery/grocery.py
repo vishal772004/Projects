c030bf73cf369e2afa340d045468254d716d07d3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-24 20:14:17 +0000

    Sun, Feb 25, 2024, 1:44 AM +05:30

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,15 +5,16 @@ while True:
         l.append(g)
     except EOFError:
         break
-z=1
+z=0
 d={}
 k=[]
 a=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 x=len(l)
 for k in l:
     for m in l:
-        if k==m and z>1:
+        if k==m:
             z=z+1
+        if z>1:
             l.remove(m)
     d[k]=z
     z=1
