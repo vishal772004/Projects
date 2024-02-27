a6e41b2ce1a3a23d50cf09013dd8d5b7657c46ab
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-27 14:27:16 +0000

    Tue, Feb 27, 2024, 7:57 PM +05:30

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,3 +1,17 @@
-
+month={
+    "January":1,
+    "February":2,
+    "March":3,
+    "April":4,
+    "May":5,
+    "June":6,
+    "July":7,
+    "August":8,
+    "September":9,
+    "October":10,
+    "November":11,
+    "December":12
+}
 date=input("Date:").split("/")
+
 
