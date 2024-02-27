0521c334aaed56a7c4b8db4940eada3311aeb3e9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-27 14:38:05 +0000

    Tue, Feb 27, 2024, 8:08 PM +05:30

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,16 +16,8 @@ date1=[]
 while True:
     date=input("Date:").split("/")
     for i in date:
-        if date[0]==i :
-            for j in month:
-                if i==j:
-                    date1[1]="0"+month[j]
-        elif date[0]==i and date[0]<=12:
-            date[1]="0"+i
-        else:
-            continue
-        print(date1)
-        break
+        print(i)
+    break
 
 
 
