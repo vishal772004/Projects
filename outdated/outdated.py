4befdab5e403b470202db264a7876c01ba3ec485
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-02-27 14:58:44 +0000

    Tue, Feb 27, 2024, 8:28 PM +05:30

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,18 +17,18 @@ while True:
     date=input("Date:")
     d1=date.split(" ")
   #  if date.isalnum():
-        if d1[1]<=31:
-            date1[2]=d1[1]
-            print(date1[2])
-        else:
-            continue
-        date1[0]=d1[-1]
-        print(date1[0])
-        for i in d1:
-            for j in month:
-                if i==j:
-                    date1[1]="0"+month[j]
-        print(date1[1])
+    if d1[1]<=31:
+        date1[2]=d1[1]
+        print(date1[2])
+    else:
+        continue
+    date1[0]=d1[-1]
+    print(date1[0])
+    for i in d1:
+        for j in month:
+            if i==j:
+                date1[1]="0"+month[j]
+    print(date1[1])
     break
 
 
