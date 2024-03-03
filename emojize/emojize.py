791cbfa58dc554068afc5c4649f7edea0ea18eda
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 10:58:28 +0000

    Sun, Mar 3, 2024, 4:28 PM +05:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,4 +1,3 @@
-import requests
-#emo=input("Input:").split()
-req=requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
-print(req.json())
+import emoji
+emo=emoji.emojize(input("Input:").split())
+
