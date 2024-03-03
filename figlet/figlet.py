e818c748aeabf77215ac6fac0f16c16bd6e3484e
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 12:27:29 +0000

    Sun, Mar 3, 2024, 5:57 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,10 +3,11 @@ from pyfiglet import Figlet
 figlet=Figlet()
 fonts=figlet.getFonts()
 
-if sys.argv[1]!="-f" or sys.argv[1]!="--font":
+if sys.argv[1]=="-f" or sys.argv[1]=="--font":
+    text=input("Input:")
+    text=figlet.setFont(font=sys.argv[2])
+    print(figlet.renderText(text))
+else:
+
     print("Invalid usage")
     sys.exit
-else:
-    text=input("Input:")
-    text=figlet.setFont(font=argv[2])
-    print(figlet.renderText(text))
