2aa7e24ccbf004edba9f6ab3cb254ecf630dcfe2
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 13:12:41 +0000

    Sun, Mar 3, 2024, 6:42 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,7 +6,8 @@ def main():
         text=input()
         text=pyfiglet.figlet_format(text,font=sys.argv[2])
         print(text)
+        sys.exit(0)
     else:
         print("Invalid usage")
-    sys.exit(1)
+        sys.exit(1)
 main()
