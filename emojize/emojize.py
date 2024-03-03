6637370fd8b8d42f478a2ba0e50b07253681e25c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 12:02:14 +0000

    Sun, Mar 3, 2024, 5:32 PM +05:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,7 +2,7 @@ import emoji
 emo=(input("Input:")).split()
 for i in emo:
     if i.startswith(":"):
-        print(emoji.emojize(i))
+        print(emoji.emojize(i),language='alias')
     else:
         print(i)
 
