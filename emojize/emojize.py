36b7eab43322705697678682c3d1fb5a0c50f115
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 10:52:20 +0000

    Sun, Mar 3, 2024, 4:22 PM +05:30

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,6 +1,6 @@
 import requests
 emo=input("Input:").split()
-req=requests.dump("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
+req=requests.get("https://carpedm20.github.io/emoji/all.html?enableList=enable_list_alias")
 for i in emo:
     if i==req:
         print(req)
