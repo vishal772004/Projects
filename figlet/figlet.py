a2b214f26e28618a09cfc0e9bb08e1d440b3eb23
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-03 12:42:03 +0000

    Sun, Mar 3, 2024, 6:12 PM +05:30

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,7 +4,7 @@ def main():
 
     if sys.argv[1]=="-f" or sys.argv[1]=="--font":
         text=input("Input:")
-        text=pyfiglet.figlet_format(text,f=sys.argv[2])
+        text=pyfiglet.figlet_format(text,font=sys.argv[2])
         print(text)
     else:
 
