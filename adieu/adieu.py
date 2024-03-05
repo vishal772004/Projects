8afd398d0d647761a4e12c0fb41a1537f640ab1a
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-05 15:21:53 +0000

    Tue, Mar 5, 2024, 8:51 PM +05:30

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -6,7 +6,7 @@ while True:
         list_of_name.append(name)
     except EOFError:
         break
-print("Adieu,adieu,to",end=" ")
+print("Adieu, adieu, to",end=" ")
 for i in list_of_name:
     if list_of_name[0]==i and len(list_of_name)<=1:
         print(i)
