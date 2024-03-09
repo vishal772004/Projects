49efef06c8f5b056aa2d5cd354496167963fdc5f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-09 14:49:27 +0000

    Sat, Mar 9, 2024, 8:19 PM +05:30

--- professor/professor.py
+++ professor/professor.py
@@ -20,14 +20,14 @@ def generate_integer(level):
         first=random.randint(0,level*10)
         second=random.randint(0,level*10)
         answer=first+second
-        print(first+"+"+second+"=",end=" ")
+        print(f"{first}+{second}=",end=" ")
         l=int(input())
         for i in range(2):
             if l==answer:
                 break
             else:
                 print("EEE")
-                print(first+"+"+second+"=",end=" ")
+                print(f"{first}+{second}=",end=" ")
                 l=int(input())
                 score=score-1
         n=n+1
