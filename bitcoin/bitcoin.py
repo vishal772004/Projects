af3a4d9dd991e6c32e15834c61adfc4d80beff69
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-09 18:14:53 +0000

    Sat, Mar 9, 2024, 11:44 PM +05:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,10 +4,11 @@ temp=""
 for i in sys.argv[1]:
     temp=temp+i
 try:
-    if temp.find(".")>0 or temp.isDigit():
-        
-
+    if temp.find(".")>0 or temp.isdigit():
+        pass
+except ValueError:
+    sys.exit()
 try:
-    ...
+    
 except requests.RequestException:
     ...
