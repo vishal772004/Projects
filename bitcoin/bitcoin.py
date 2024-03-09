e447ccf1ca478b0f7fa11ee235d6ea7164b56b0a
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-09 18:46:32 +0000

    Sun, Mar 10, 2024, 12:16 AM +05:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,6 +1,8 @@
 import requests
 import sys
+import math
 temp=""
+price=[]
 try:
     for i in sys.argv[1]:
         temp=temp+i
@@ -10,9 +12,16 @@ except ValueError:
     sys.exit()
 except IndexError:
     print("Missing Command-line Argument")
+    z,y=0
 try:
     details=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
-    price=float(details['bpi']['USD']['rate'])
+    for i in details['bpi']['USD']['rate']:
+        if i=="." or z>0:
+            z=z+1
+            price=price+(float(i)*(math.pow(10,-z)))
+        else:
+            
+            price=price+(float(i))
     print("$",price*sys.argv[1])
 except requests.RequestException:
     sys.exit()
