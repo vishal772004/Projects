3cc6a481db9ec8776205a6a92df6c68a9bf353ab
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-09 18:19:12 +0000

    Sat, Mar 9, 2024, 11:49 PM +05:30

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,15 +1,17 @@
 import requests
 import sys
 temp=""
-for i in sys.argv[1]:
-    temp=temp+i
 try:
+    for i in sys.argv[1]:
+        temp=temp+i
     if temp.find(".")>0 or temp.isdigit():
         pass
 except ValueError:
     sys.exit()
+except IndexError:
+    print("Missing Command-line Argument")
 try:
-    details=requests.json()
+    details=requests.get()
     print(details)
 except requests.RequestException:
     sys.exit()
