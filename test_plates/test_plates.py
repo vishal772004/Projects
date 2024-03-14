6614477152de2ddf5744538cc1676291f0a85fb3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-14 14:37:39 +0000

    Thu, Mar 14, 2024, 8:07 PM +05:30

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,12 +1,17 @@
-from plates import isvalid
+from plates import is_valid
 
 def test_length():
-    assert isvalid("ABC123")==True
+    assert is_valid("ABC1234")==False
+    assert is_valid("A")==False
 
 def test_not_alphanumeric():
-    assert isvalid("!@#$%&*")==False
+    assert is_valid("!@#$%&*")==False
 
 def test_not_starting_from_zero():
-    assert isvalid("ABC012")==False
+    assert is_valid("ABC012")==False
 
-def 
+def test_ending_with_digit():
+    assert is_valid("ABC12A")==False
+
+def test_starts_with_alphabet():
+    assert is_valid("12ABCD")==False
