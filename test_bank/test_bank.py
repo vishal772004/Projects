643563a7376205d5fdd806515412eb6ef84672b0
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-14 14:21:46 +0000

    Thu, Mar 14, 2024, 7:51 PM +05:30

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,4 +1,8 @@
 from bank import value
 
 def test_fullgreeting():
-    assert value("Hello")==
+    assert value("Hello")==0
+def test_halfgreeting():
+    assert value("Hey")==20
+def test_greeting_not_including_h():
+    assert value("What's up")==100
