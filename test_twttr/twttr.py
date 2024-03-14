aab45bd074df893c1590af6b4bc7a2c45bb8d096
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-14 13:56:58 +0000

    Thu, Mar 14, 2024, 7:26 PM +05:30

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,11 +1,15 @@
 
-phrase=input("Input:")
-l=[]
-for i in phrase:
-    l.append(i)
-vowels=["A","E","I","O","U","a","e","i","o","u"]
-for i in phrase:
-    for j in vowels:
-        if i==j:
-            phrase=phrase.replace(i,"")
-print("Output:",phrase)
+def main():
+    phrase=input("Input:")
+    l=[]
+    for i in phrase:
+        l.append(i)
+    print(shorten(phrase))
+def shorten(word):
+    vowels=["A","E","I","O","U","a","e","i","o","u"]
+    for i in word:
+        for j in vowels:
+            if i==j:
+                word=word.replace(i,"")
+    return word
+
