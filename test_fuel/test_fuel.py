87bfc653da6dc2ce43a75ea9837aa2570539d2b2
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 12:31:05 +0000

    Sat, Mar 16, 2024, 6:01 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -7,6 +7,7 @@ def test_value_error():
     pytest.raises(ValueError)
 def test_empty():
     assert gauge(0)=="E"
+    assert gauge(1)=="E"
 def test_full():
     assert gauge(100)=="F"
     assert gauge(99)=="F"
@@ -17,5 +18,7 @@ def test_fraction():
     assert convert("3/4")==75
     assert convert("1/2")==50
     assert convert("0/1")==0
+    assert convert("1/100")==1
+    assert convert("99/100")==0
 
 
