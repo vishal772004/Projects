7e0932246691dd8370ee19e8fb7bb0546c881328
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 12:36:07 +0000

    Sat, Mar 16, 2024, 6:06 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -2,7 +2,8 @@ import pytest
 from fuel import convert,gauge
 
 def test_zero_division():
-    pytest.raises(ZeroDivisionError)
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
 def test_value_error():
     pytest.raises(ValueError)
 def test_empty():
