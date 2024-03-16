bb7a5956d66d18d1d97998d2b5f8dae85ab310c7
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 11:34:44 +0000

    Sat, Mar 16, 2024, 5:04 PM +05:30

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -6,27 +6,33 @@ def main():
                 continue
             else:
                 pass
-            l=first.split("/")
-            x=int(l[0])
-            y=int(l[-1])
-            if x>y:
-                continue
-            p=(x/y)*100
 
+            p=convert(first)
+            gauge(p)
             break
         except ValueError:
             continue
         except ZeroDivisionError:
             continue
+def gauge(p):
+    if p<=1:
+        print("E")
+    elif p>=99:
+        print("F")
+    elif (p-int(p)>=0.5):
+        print(int(p+1),"%",sep="")
+    else:
+        print(int(p),"%",sep="")
 
-if p<=1:
-    print("E")
-elif p>=99:
-    print("F")
-elif (p-int(p)>=0.5):
-    print(int(p+1),"%",sep="")
-else:
-     print(int(p),"%",sep="")
+def convert(first):
+    l=[]
+    l=first.split("/")
+    x=int(l[0])
+    y=int(l[-1])
+    if x>y:
+        continue
+    p=(x/y)*100
+    return p
 
-
-
+if __name__=="__main__":
+    main()
