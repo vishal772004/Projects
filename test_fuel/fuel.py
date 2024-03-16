d70ef87e304d0511576ea211fd35f6e3dcbb00d4
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 11:54:45 +0000

    Sat, Mar 16, 2024, 5:24 PM +05:30

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -2,33 +2,34 @@ def  main():
     while True:
         try:
             first=input("Fraction:")
-            convert(first)
-
+            if first.find(".")>=0:
+                continue
+            else:
+                pass
+            l=first.split("/")
+            x=int(l[0])
+            y=int(l[-1])
+            if x>y:
+                continue
+            x=convert(x,y)
+            gauge(x)
             break
         except ValueError:
             continue
         except ZeroDivisionError:
             continue
-def convert(first):
-    if first.find(".")>=0:
-        continue
-    else:
-        pass
-    l=first.split("/")
-    x=int(l[0])
-    y=int(l[-1])
-    if x>y:
-        continue
+def convert(x,y):
     return (x/y)*100
 
-if p<=1:
-    print("E")
-elif p>=99:
-    print("F")
-elif (p-int(p)>=0.5):
-    print(int(p+1),"%",sep="")
-else:
-     print(int(p),"%",sep="")
+def gauge(p):
+    if p<=1:
+        print("E")
+    elif p>=99:
+        print("F")
+    elif (p-int(p)>=0.5):
+        print(int(p+1),"%",sep="")
+    else:
+        print(int(p),"%",sep="")
 
 
 
