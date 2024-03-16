792ac7a960a7ca58f711eeeed105f9cbd06a88d0
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 10:59:56 +0000

    Sat, Mar 16, 2024, 4:29 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,6 +5,6 @@ def test_zero_division():
     with pytest.raises(ZeroDivisionError):
         1/0
 def test_value_error():
-    pytest.raises(ValueError, convert, 0<convert(50)<100)
+    pytest.raises(ValueError)
 
 
