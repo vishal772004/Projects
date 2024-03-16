5e64ce3c766077f3c442b457f984a76725864f62
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-16 13:30:36 +0000

    Sat, Mar 16, 2024, 7:00 PM +05:30

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -8,6 +8,15 @@ def test_value_error():
     with pytest.raises(ValueError):
         convert("cat/dog")
         convert("5/3")
+def test_valid():
+    assert convert("0/1")==0 and gauge(0)=="E"
+    assert convert("99/100")==99 and gauge(99)=="F"
+    assert convert("1/100")==1 and gauge(1)=="E"
+    assert convert("3/4")==75 and gauge(75)=="75%"
+    assert convert("1/2")==50 and gauge(50)=="50%"
+
+
+
 
 
 
