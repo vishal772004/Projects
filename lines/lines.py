141ca9b51e124f287c3c808f55b2818fed52b17a
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-23 11:43:28 +0000

    Sat, Mar 23, 2024, 5:13 PM +05:30

--- lines/lines.py
+++ lines/lines.py
@@ -5,7 +5,6 @@ try:
         pass
     elif not(sys.argv[1].endswith(".py")):
         print("Not a python file")
-        sys.exit()
     with open(sys.argv[1]) as file:
         for line in file:
             if line.startswith("# ") or line.isspace():
@@ -14,10 +13,8 @@ try:
     print(l)
 except IndexError:
     print("Too few command-line arguments")
-    sys.exit()
 except FileNotFoundError:
     print("File does not exist")
-    sys.exit()
 
 
 
