ae1545f6bd10ffaa17457cdcfd9d406a5b020dad
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-23 11:32:28 +0000

    Sat, Mar 23, 2024, 5:02 PM +05:30

--- lines/lines.py
+++ lines/lines.py
@@ -1,10 +1,24 @@
 import sys
-
+l=0
 try:
     if sys.argv[1].endswith(".py"):
-        print("it works")
+        pass
     elif not(sys.argv[1].endswith(".py")):
         print("Not a python file")
+        sys.exit()
+    with open(sys.argv[1]) as file:
+        for line in file:
+            if line.startswith("#"):
+                continue
+            l=l+1
+    print(l)
 except IndexError:
     print("Too few command-line arguments")
+    sys.exit()
+except FileNotFoundError:
+    print("File does not exist")
+    sys.exit()
+
+
+
 
