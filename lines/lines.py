c086dac2c0a5e32a863b41b90a797657f5f04aaf
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-23 12:46:15 +0000

    Sat, Mar 23, 2024, 6:16 PM +05:30

--- lines/lines.py
+++ lines/lines.py
@@ -7,10 +7,11 @@ def main():
         elif not(sys.argv[1].endswith(".py")):
             sys.exit("Not a python file")
         with open(sys.argv[1]) as file:
-            for line in file:
-                if line.startswith("#") or line.isspace():
-                    continue
-                l=l+1
+            lines=file.readlines()
+        for line in lines:
+            if line.startswith("#") or line.isspace():
+                continue
+            l=l+1
         if len(sys.argv)==3:
             sys.exit("Too many command-line arguments")
         print(l)
