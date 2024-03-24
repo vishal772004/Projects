c6ea8c41765c35fdc80b8f6a5f639ccb4052c2c7
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 17:13:56 +0000

    Sun, Mar 24, 2024, 10:43 PM +05:30

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,4 +5,9 @@ def main():
     elif len(sys.argv)>2:
         sys.exit("Too many command line arguments")
     else:
-        
+        if sys.argv[1].endswith(".jpg") and sys.argv[2].endswith(".jpg"):
+            sys.exit("Invalid input")
+
+        if not(sys.argv[1].endswith(".jpg") and sys.argv[2].endswith(".jpg")):
+            sys.exit("Input and output have different extensions")
+
