a7ef297e4a71c805497d07a953fb39b452a2fb44
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 15:31:57 +0000

    Sun, Mar 24, 2024, 9:01 PM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -6,21 +6,24 @@ def main():
     elif len(sys.argv)>3:
         sys.exit("Too many command line arguments")
     else:
-        if not(sys.argv[1].endswith(".csv")):
-            sys.exit("Could not read",sys.argv[1])
-        before,after=[],[]
-        with open(sys.argv[1]) as file:
-            read=csv.DictReader(file)
-            for i in read:
-                before.append(i)
-        with open(sys.argv[2],"w") as file1:
-            writer = csv.DictWriter(file1, fieldnames=['first','last','house'])
-            writer.writeheader()
-            for i in before:
-                str=i['name']
-                str=str.split(",")
-                after.append({'first':str[1],'last':str[0],'house':i['house']})
-            writer.writerows(after)
+        try:
+            if not(sys.argv[1].endswith(".csv")):
+                sys.exit("Could not read",sys.argv[1])
+            before,after=[],[]
+            with open(sys.argv[1]) as file:
+                read=csv.DictReader(file)
+                for i in read:
+                    before.append(i)
+            with open(sys.argv[2],"w") as file1:
+                writer = csv.DictWriter(file1, fieldnames=['first','last','house'])
+                writer.writeheader()
+                for i in before:
+                    str=i['name']
+                    str=str.split(",")
+                    after.append({'first':str[1],'last':str[0],'house':i['house']})
+                writer.writerows(after)
+        except FileNotFoundError:
+            sys.exit("Could not find the csv file")
 if __name__=="__main__":
     main()
 
