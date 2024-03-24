5078daa9a4ae35b923d1ab4bef42238ba7ca7fe7
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 03:54:30 +0000

    Sun, Mar 24, 2024, 9:24 AM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -8,9 +8,11 @@ def main():
     else:
         if not(sys.argv[1].endswith(".csv")):
             sys.exit("Could not read",sys.argv[1])
+        hogwarts=[]
         with open(sys.argv[1]) as file:
             read=csv.DictReader(file)
-        print(read)
+            hogwarts.append(read)
+        print(hogwarts)
 
 if __name__=="__main__":
     main()
