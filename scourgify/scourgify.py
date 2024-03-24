52a59df5721b594d49b2a25cf674ad725e8ba997
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 03:45:26 +0000

    Sun, Mar 24, 2024, 9:15 AM +05:30

---
+++ scourgify/scourgify.py
@@ -0,0 +1,9 @@
+import sys
+def main():
+    if len(sys.argv)<2:
+        sys.exit("Too few command line arguments")
+    elif len(sys.argv)>2:
+        sys.exit("Too many command line arguments")
+    else:
+        
+
