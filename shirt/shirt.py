21f15114ef201b5fe5b7c672cda2d8d945e7a450
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 17:03:31 +0000

    Sun, Mar 24, 2024, 10:33 PM +05:30

---
+++ shirt/shirt.py
@@ -0,0 +1,8 @@
+import sys
+def main():
+    if len(sys.argv)<2:
+        sys.exit("Too few command line arguments")
+    elif len(sys.argv)>2:
+        sys.exit("Too many command line arguments")
+    else:
+        
