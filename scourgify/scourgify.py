018cb745ec6c0ea86344f7566cff049f06f2afd2
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 14:54:28 +0000

    Sun, Mar 24, 2024, 8:24 PM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -17,8 +17,8 @@ def main():
             writer = csv.DictWriter(file1, fieldnames=['name','house'])
             writer.writeheader()
             #writer.writerows(hogwarts)
-            for 'name' in hogwarts:
-                print()
+            for name in hogwarts:
+                print(name)
 if __name__=="__main__":
     main()
 
