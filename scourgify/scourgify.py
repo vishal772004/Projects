9fbcb1c59a1984dcc8a01cae347d16f89d9d4fa6
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 03:47:10 +0000

    Sun, Mar 24, 2024, 9:17 AM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,5 +5,8 @@ def main():
     elif len(sys.argv)>2:
         sys.exit("Too many command line arguments")
     else:
+        if not(sys.argv[1].endswith(".csv")):
+            sys.exit("Could not read",sys.argv[1])
         
+
 
