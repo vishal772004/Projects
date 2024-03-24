63ab4ffc90cc15d78ed8a5adb45c99599fe29859
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-24 03:52:57 +0000

    Sun, Mar 24, 2024, 9:22 AM +05:30

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 def main():
-    if len(sys.argv)<2:
+    if len(sys.argv)<3:
         sys.exit("Too few command line arguments")
     elif len(sys.argv)>3:
         sys.exit("Too many command line arguments")
