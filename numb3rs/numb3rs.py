fe8343c43f6dda9ac577421fddb9cc84d3dd7b8b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 15:25:09 +0000

    Thu, Mar 28, 2024, 8:55 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -8,8 +8,8 @@ def main():
 
 def validate(ip):
     ipv4=re.search(r"^(\d)\.(\d)\.(\d)\.(\d)$",ip)
-    for i in range(1,3):
-        if 0<int(ipv4.group(i))<=255:
+    for i in range(1,4):
+        if int(ipv4.group(i))>=255:
             return False
     return True
 
