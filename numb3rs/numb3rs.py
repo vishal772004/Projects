1de89eb950db003fc0cfef927f37bc7884577b08
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 14:20:33 +0000

    Thu, Mar 28, 2024, 7:50 PM +05:30

---
+++ numb3rs/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if re.search(,ip,)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
