22ed2b413ed7dee21cf2897ba0db9b465decaba0
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 14:23:46 +0000

    Thu, Mar 28, 2024, 7:53 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,7 +7,10 @@ def main():
 
 
 def validate(ip):
-    if re.search("[0-255]*\.[0-255]*\.",ip):
+    if re.search(r"^[0-255]*\.[0-255]*\.[0-255]*\.[0-255]*$",ip):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
