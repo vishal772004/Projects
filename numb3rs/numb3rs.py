89d6efe2a37589e8d50e127386ff017e1b88c819
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 15:42:06 +0000

    Thu, Mar 28, 2024, 9:12 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,10 +1,7 @@
 import re
 import sys
-
-
 def main():
     print(validate(input("IPv4 Address: ")))
-
 
 def validate(ip):
     ipv4=re.search(r"^(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)$",ip)
@@ -16,7 +13,6 @@ def validate(ip):
         if li[i]>255:
             return False
     return True
-
 
 if __name__ == "__main__":
     main()
