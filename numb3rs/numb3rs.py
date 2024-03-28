efedb78c86ef524c238fbf55dacbfd6aca1c50e2
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 15:44:48 +0000

    Thu, Mar 28, 2024, 9:14 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -4,15 +4,18 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    ipv4=re.search(r"^(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)$",ip)
-    li=[]
-    for i in range(1,5):
-        v=ipv4.group(i)
-        li.append(int(v))
-    for i in range(len(li)):
-        if li[i]>255:
-            return False
-    return True
+    try:
+        ipv4=re.search(r"^(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)\.(\d*\d*\d*)$",ip)
+        li=[]
+        for i in range(1,5):
+            v=ipv4.group(i)
+            li.append(int(v))
+        for i in range(len(li)):
+            if li[i]>255:
+                return False
+        return True
+    except AttributeError:
+        return False
 
 if __name__ == "__main__":
     main()
