d6ebb74e6b35b471a5dda1cf19dce51dd074d710
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-28 15:31:30 +0000

    Thu, Mar 28, 2024, 9:01 PM +05:30

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 
 def validate(ip):
-    ipv4=re.search(r"^(\d)\.(\d)\.(\d)\.(\d)$",ip)
+    ipv4=re.search(r"^(\d)+\.(\d)+\.(\d)+\.(\d)+$",ip)
     li=[]
     for i in range(1,4):
         v=ipv4.group(i)
