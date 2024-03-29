fa1e74b4f0e1efa8d5bec8149cc84f22812617ac
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 09:06:26 +0000

    Fri, Mar 29, 2024, 2:36 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -5,7 +5,7 @@ def main():
 
 
 def parse(s):
-    link=re.search(r"^<\w+ \w+=.+\w+://(\w+\.\w+\.\w+)/\w+/(\w+).+></\w+>$",s)
+    link=re.search(r"^<\w+ \w+=\"\w+://(\w+\.\w+\.\w+)/\w+/(\w+)\"></\w+>$",s)
     if link.group(1)!="www.youtube.com":
         return "None"
     watchlink="htpps://youtu.be/"+link.group(2)
