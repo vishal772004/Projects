57fa0f88dc2569df59e6763e9fc65a2224a77bc2
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 11:40:36 +0000

    Fri, Mar 29, 2024, 5:10 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -8,8 +8,8 @@ def parse(s):
     if re.search("<iframe .*></iframe>",s):
         if link:=re.search("src=\"(http|htpps)?://www\.youtube\.com/embed/(\w+)\"",s):
             watchlink="htpps://youtu.be/"+link.group(2)
-        else:
-            return "None"
+    else:
+        return "None"
     return watchlink
 
 
