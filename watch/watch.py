f7eb4bb523e809bd518fe163dd8d66474e04aa73
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 12:28:54 +0000

    Fri, Mar 29, 2024, 5:58 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -9,7 +9,7 @@ def parse(s):
         link=re.search(r"(?:(?:http|https)*:\/\/(?:www\.)*youtube\.com\/embed\/)(\w)+",s)
         if link:
             url=link.group()
-            return "https://youtu.be/"+url
+            return "https://youtu.be/"+url[-1]
 
 if __name__ == "__main__":
     main()
