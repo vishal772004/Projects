066666c1daad33c440d93d9af436b1576bacf1c6
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 07:14:39 +0000

    Fri, Mar 29, 2024, 12:44 PM +05:30

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -3,4 +3,8 @@ from numb3rs import validate
 
 def valid_ip():
     assert validate("1.2.3.4") ==True
-    assert validate()
+    assert validate("255.255.255.255")==True
+    assert validate("10.200.199.37")==True
+def invalid_ip():
+    assert validate("300.274.574.256")==False
+def 
