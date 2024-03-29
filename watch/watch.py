c79fe98e0c40608b6d44abf2a0019c752b7659ee
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 08:06:17 +0000

    Fri, Mar 29, 2024, 1:36 PM +05:30

--- watch/watch.py
+++ watch/watch.py
@@ -8,10 +8,7 @@ def main():
 
 def parse(s):
     link=re.search(r"src=\"htpps://www.youtube.com/\w+/(\w+)",s)
-    if link:
-        watchlink="htpps://youtu.be/"+link.group(1)
-    else:
-        return "None"
+    watchlink="htpps://youtu.be/"+link.group(1)
     return watchlink
 
 
