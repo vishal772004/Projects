d153745ba5a0d5031593028ef0459b26e32bbe97
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-29 07:26:34 +0000

    Fri, Mar 29, 2024, 12:56 PM +05:30

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,13 +1,13 @@
 import pytest
 from numb3rs import validate
 
-def valid_ip():
+def test_valid_ip():
     assert validate("1.2.3.4") ==True
     assert validate("255.255.255.255")==True
     assert validate("10.200.199.37")==True
-def invalid_ip():
+def test_invalid_ip():
     assert validate("300.274.574.256")==False
-def invalid():
+def test_invalid():
     with pytest.raises(AttributeError):
         validate("cat")
 
