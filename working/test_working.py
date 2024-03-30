d0f9aa73d19766d8fa2fb8d110bc74c6d1e59da1
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 04:08:31 +0000

    Sat, Mar 30, 2024, 9:38 AM +05:30

--- working/test_working.py
+++ working/test_working.py
@@ -5,4 +5,5 @@ def test_validtime():
     assert convert("9:00 AM to 9:00 PM")==("9:00 to 21:00")
     assert convert("12:00 AM to 12:00 PM")==("12:00 to 12:00")
     assert convert("10:30 AM to 5:20 PM")==("10:30 to 17:20")
+    assert convert("9 AM to 9 PM")==("9:00 to 21:00")
 
