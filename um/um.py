aa857c7819a0abe674a4a1cfa2c54e24343e343c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 12:19:58 +0000

    Sat, Mar 30, 2024, 5:49 PM +05:30

--- um/um.py
+++ um/um.py
@@ -8,7 +8,7 @@ def count(s):
     counting=0
     words=s.split()
     for i in words:
-        if re.search("[ ,]*(um|Um)[ ,]*",i):
+        if re.search("^[ ,]*(um|Um)[ ,]*$",i):
             counting=counting+1
     return counting
 
