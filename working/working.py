f7deb7be5587fbef4650512cf8fb3ecb8534f6cd
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 11:11:02 +0000

    Sat, Mar 30, 2024, 4:41 PM +05:30

--- working/working.py
+++ working/working.py
@@ -6,13 +6,21 @@ def main():
 
 def convert(s):
   #  try:
-        if time:=re.search("^([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+ to ([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+$",s):
+        if re.search("^([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+ to ([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+$",s):
             li=s.split(" to ")
             for i in range(len(li)):
-                if li[i].find(":")<=0:
-                    hour,minute=li[i].split(":")
+                time,meridian=li[i].split(" ")
+                if time.find(":")>0:
+                    hour,minute=time.split(":")
                 else:
-                    hour=li[i]
+                    hour=time
+                if meridian=="PM":
+                     r=int(hour)+12
+                     if hour!="12":
+                        li[i]=li[i].replace(hour,str(r))
+                     li[i]=li[i].replace("PM","")
+
+
 
 
 if __name__ == "__main__":
