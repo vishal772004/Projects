5e051505355001fdc86aa57b378c84b641b6c8da
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 10:53:10 +0000

    Sat, Mar 30, 2024, 4:23 PM +05:30

--- working/working.py
+++ working/working.py
@@ -12,7 +12,7 @@ def convert(s):
                 if li[i].find("PM")>0 :
                     r=int(li[i][0])+12
                     li[i]=li[i].replace("PM","")
-                    li[i]=li[i].replace(li[i][0],str(r))
+                    
                 li[i]=li[i].replace("AM","")
             for i in range(len(li)):
                 if li[i].find(":")<=0:
