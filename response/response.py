dc3638137dafe1d0db8102707f835cb4332f96b4
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 12:37:56 +0000

    Sat, Mar 30, 2024, 6:07 PM +05:30

--- response/response.py
+++ response/response.py
@@ -1,11 +1,12 @@
-import re
 
+import validators
 def main():
     mail=input("What's your email address?")
     print(validate(mail))
 
 def validate(s):
-    if re.search("^[a-zA-Z0-9]*@[a-zA-Z0-9]*\.com$",s,re.IGNORECASE):
+   # if re.search("^[a-zA-Z0-9]*@[a-zA-Z0-9]*\.com$",s,re.IGNORECASE):
+    if validators.email("someone@example.com")
         return "Valid"
     else:
         return "Invalid"
