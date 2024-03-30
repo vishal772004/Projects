6d141939107bee676fc0f717ba84465c80c6c588
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 12:04:43 +0000

    Sat, Mar 30, 2024, 5:34 PM +05:30

--- um/um.py
+++ um/um.py
@@ -8,7 +8,7 @@ def count(s):
     counting=0
     words=s.split()
     for i in words:
-        if "um" in i and re.search("[^a-zA-Z]*um[^a-zA-Z]*",i):
+        if re.search("[^a-zA-Z]*um[^a-zA-Z]*",i,):
             counting=counting+1
     return counting
 
