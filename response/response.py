30f3f91597dc8bb0c02f633268f323d37417bd43
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 12:29:35 +0000

    Sat, Mar 30, 2024, 5:59 PM +05:30

--- response/response.py
+++ response/response.py
@@ -2,12 +2,13 @@ import re
 
 def main():
     mail=input("What's your email address?")
-    
+    print(validate(mail))
 
-
-
-
-
+def validate(s):
+    if re.search("^[a-zA-Z0-9_]*@[a-zA-Z0-9]\.com$",s,re.IGNORECASE):
+        return "Valid"
+    else:
+        return "Invalid"
 
 if __name__=="__main__":
     main()
