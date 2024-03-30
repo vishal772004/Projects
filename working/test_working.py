028d9ef42df6759984fcdde1f5bd84872de1febd
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 11:47:25 +0000

    Sat, Mar 30, 2024, 5:17 PM +05:30

--- working/test_working.py
+++ working/test_working.py
@@ -6,13 +6,15 @@ def test_validtime():
     assert convert("12:00 AM to 12:00 PM")==("00:00 to 12:00")
     assert convert("10:30 AM to 5:20 PM")==("10:30 to 17:20")
     assert convert("9 AM to 9 PM")==("09:00 to 21:00")
-def test_inavlidtime():
+def test_incorrecthours():
     with pytest.raises(ValueError):
         convert("13:00 AM to 13:00 PM")
-        convert("9:00 AM - 9:00 PM")
-        convert("12 AM - 12 PM")
-        convert("12 AM 12 PM")
-        convert("25 to 36")
-        convert("10:60 AM to 12:78 PM")
         convert("13 PM to 17 PM")
+def test_incorrectminutes():
+    with pytest.raises(ValueError):
+        convert("10:69 AM to 6:97 PM")
+
+def test_omitto():
+    with pytest.raises(ValueError):
+        convert("9 AM - 9 PM")
 
