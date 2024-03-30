1324c9e86732cf90269eac402f88711d9dccae8e
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 03:45:08 +0000

    Sat, Mar 30, 2024, 9:15 AM +05:30

--- working/working.py
+++ working/working.py
@@ -7,13 +7,13 @@ def main():
 def convert(s):
     if time:=re.search("^[0-9]+(:[0-9]+) (AM|PM)+ to [0-9]+(:[0-9]+) (AM|PM)+$",s):
         li=s.split(" to ")
-        for i in li:
+        for i in range(len(li)):
             if i.find("PM")>0:
                 r=int(li[i][0])+12
                 li[i]=li[i].replace("PM","")
                 li[i]=li[i].replace(li[i][0],str(r))
             li[i]=li[i].replace("AM","")
-            
+        return li[0]+li[1]
 
 
 
