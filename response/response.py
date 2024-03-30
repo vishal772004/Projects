5b5a0cadc66aa75d1a22e5740ebf18db88361d92
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 12:43:51 +0000

    Sat, Mar 30, 2024, 6:13 PM +05:30

--- response/response.py
+++ response/response.py
@@ -6,9 +6,10 @@ def main():
 
 def validate(s):
    # if re.search("^[a-zA-Z0-9]*@[a-zA-Z0-9]*\.com$",s,re.IGNORECASE):
-    if validators.email("someone@example.com"):
+    try:
+        valid=validators.email("someone@example.com")
         return "Valid"
-    else:
+    except ValueError:
         return "Invalid"
 
 if __name__=="__main__":
