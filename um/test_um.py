cda9d40a2892f733b563ffdd81774db5519daed8
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 11:58:29 +0000

    Sat, Mar 30, 2024, 5:28 PM +05:30

--- um/test_um.py
+++ um/test_um.py
@@ -2,9 +2,9 @@
 from um import count
 
 def count_once():
-    assert convert("um")==1
-    assert convert("um?")==1
-    assert convert("Um, thanks for the album")==1
+    assert count("um")==1
+    assert count("um?")==1
+    assert count("Um, thanks for the album")==1
 
 def count_twice():
-    assert convert("Um, thanks, um...")==2
+    assert count("Um, thanks, um...")==2
