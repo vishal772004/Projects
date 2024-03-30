82f309d954f92ea6f4f21770372b7638708d0d44
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-03-30 04:19:48 +0000

    Sat, Mar 30, 2024, 9:49 AM +05:30

--- working/working.py
+++ working/working.py
@@ -5,7 +5,7 @@ def main():
 
 
 def convert(s):
-    try:
+  #  try:
         if time:=re.search("^([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+ to ([1-9]|[1][0-2])?(:[0-5][0-9])? (AM|PM)+$",s):
             li=s.split(" to ")
             for i in range(len(li)):
@@ -19,8 +19,8 @@ def convert(s):
                     li[i]=li[i]+":00"
             return li[0]+"to "+li[1].strip()
         raise ValueError
-    except ValueError:
-        return None
+   # except ValueError:
+     #   return None
 
 
 
