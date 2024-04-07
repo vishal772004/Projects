8c101f6a4cd357b0fead67cbf7e6f5f03036cdae
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 19:12:28 +0000

    Mon, Apr 8, 2024, 12:42 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,7 +5,8 @@ def main():
     p=inflect.engine()
     date_of_birth=check(birth_date)
     today=datetime.date.today()
-    total_minutes=convert(date_of_birth,today)
+    final=convert(date_of_birth,today)
+    total_minutes=final.days
     words=p.number_to_words(total_minutes,andword=", ").lstrip("minus ")
     print(words,"minutes")
 
@@ -21,7 +22,6 @@ def check(birth_date):
     date_=datetime.date(int(year),int(month),int(date))
     return date_
 def convert(date_of_birth,today):
-    print(type(today))
     final=today-date_of_birth
     total_minutes=-(final*24*60)
     return total_minutes
