60ef1520a090eb463c200659f609ca915c3d8ea3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 19:29:28 +0000

    Mon, Apr 8, 2024, 12:59 AM +05:30

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,6 +2,8 @@ import pytest
 from seasons import check
 
 def test_validdate():
-    assert check("2024","04","07")==("2024-04-07")
+    assert check("2024-04-07")==("2024","04","07")
+    assert check("2023-04-07")==("2023","04","07")
+    
 
 
