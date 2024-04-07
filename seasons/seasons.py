9f4effc26061245abad87c27ddcbee6b040b556d
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 14:00:04 +0000

    Sun, Apr 7, 2024, 7:30 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,23 +4,19 @@ from datetime import date
 def main():
     try:
         year,month,date = input("Date of Birth:").split("-")
-
     except ValueError:
         print("Invalid date")
 
-    date=Datetime(date,month,year)
-    date.MINYEAR("1900")
-    date.calender()
+    Date=Datetime(date,month,year)
+
+
 
 class Datetime():
     def __init__(self,date,month,year):
-        self.date = date
-        self.month = month
-        self.year = year
-
-    def calender(self):
-        return f"Date :{self.date}-Month :{self.month}-Year :{self.year}"
-
+        self.date=date
+        self.month=month
+        self.year=year
+        print(f"{self.date} is  the date and {self.month} is the month and {self.year} is the year")
 
 if __name__ == "__main__":
     main()
