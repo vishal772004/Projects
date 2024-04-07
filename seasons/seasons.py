a45a5bd49e1525e88e0a1905bae3bece8c9e8fcd
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 19:05:15 +0000

    Mon, Apr 8, 2024, 12:35 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,25 +1,24 @@
 import datetime,sys,inflect,re
 
 def main():
-
+    birth_date=input("Date of Birth:")
     p=inflect.engine()
-    dob=Birthdate(birth_date)
-    date_of_birth=dob.check(birth_date)
+    date_of_birth=check(birth_date)
     today=datetime.date.today()
     total_minutes=convert(date_of_birth,today)
     words=p.number_to_words(total_minutes,andword=", ").lstrip("minus ")
     print(words,"minutes")
 
-def check():
+def check(birth_date):
     try:
-        birth_date=input("Date of Birth:")
         if re.search("^[0-9][0-9][0-9][0-9]-([0][0-9]||[1][0-2])+-([0][0-9]||[0-3][0-9])+$",birth_date):
             pass
         else:
             raise ValueError
     except ValueError:
         sys.exit("Invalid Date")
-    date_=datetime.date(int(self.year),int(self.month),int(self.date))
+    year,month,date=birth_date.split("-")
+    date_=datetime.date(int(year),int(month),int(date))
     return date_
 def convert(date_of_birth,today):
     print(type(today))
--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,4 +4,8 @@ from seasons import check
 def test_validdate():
     assert check("2024-04-07")==("2024-04-07")
 
+def test_invaliddate():
+    with pytest.raises(ValueError):
+        ("January 20,2001")
+
 
