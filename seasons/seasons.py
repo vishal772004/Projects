85a31528e09fdcf5964a3c6b2adc99ccf2df6722
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 03:31:43 +0000

    Sun, Apr 7, 2024, 9:01 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -9,6 +9,7 @@ def main():
 
     d=Datetime(date,month,year)
     d.__str__()
+    print(f"Date :{self.date}-Month :{self.month}-Year :{self.year}")
 
 class Datetime():
     def __init__(self,date,month,year):
