5565c8372463f6eba0e105e4f202626849eeec9e
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 17:47:47 +0000

    Sun, Apr 7, 2024, 11:17 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,40 +1,11 @@
 import datetime,sys,inflect
 
 def main():
-    p = inflect.engine()
-    try:
-        _date=Datetime.get()
-    except ValueError:
-        sys.exit("Invalid Date")
-    date_=Datetime.gettoday()
-    _date=datetime.date(_date)
-    print(_date)
+    birth_date=input("Date of Birth:")
+
+class Birthdate:
+    def __init
 
 
-class Datetime():
-    def __init__(self,date,month,year):
-        self.date=int(date)
-        self.month=int(month)
-        self.year=int(year)
-    def __str__(self):
-        return f"{self.year}-{self.month}-{self.date}"
-    
-
-    def __sub__(self,other):
-        date = self.date-other.date
-        month = self.month-other.month
-        year = self.year-other.year
-        return Datetime(date,month,year)
-
-    @classmethod
-    def gettoday(cls):
-        year,month,date=str(datetime.date.today()).split("-")
-        return cls(year,month,date)
-    @classmethod
-    def get(cls):
-        year,month,date =input("Date of Birth:").split("-")
-        return cls(date,month,year)
-
-
-if __name__ == "__main__":
+if __name__ =="__main__":
     main()
