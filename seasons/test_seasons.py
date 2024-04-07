74e81c14e04624b0ab46e8bd1d1b1974db28d600
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 18:48:54 +0000

    Mon, Apr 8, 2024, 12:18 AM +05:30

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,6 +2,7 @@ import pytest
 from seasons import convert
 
 def test_validdate():
-    assert convert((2024-4-6),(2024-4-7))==1440
-    assert convert((2023-4-7),(2024-4-7))==525600
+    assert convert((2024-4-7),(2024-4-8))==1440
+    assert convert((2024-4-6),(2024-4-8))==2880
+    assert convert((2023-4-7),(2024-4-8))==525600
 
