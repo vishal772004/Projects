bfa70151456dce8d3964b949d526a4516f740d87
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 17:50:34 +0000

    Sun, Apr 7, 2024, 11:20 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,10 +1,16 @@
 import datetime,sys,inflect
 
 def main():
-    birth_date=input("Date of Birth:")
+    try:
+        birth_date=input("Date of Birth:")
+    except ValueError:
+        sys.exit("Invalid Date")
 
 class Birthdate:
-    def __init
+    def __init__(self,birth_date):
+        self.year,self.month,self.date=birth_date.split("-")
+
+
 
 
 if __name__ =="__main__":
