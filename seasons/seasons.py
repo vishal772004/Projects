cc1476376e71e13e997f34b721a700342d1c36e9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 15:20:51 +0000

    Sun, Apr 7, 2024, 8:50 PM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -8,17 +8,18 @@ def main():
         sys.exit("Invalid Date")
     date_=Datetime.gettoday()
     final = _date-date_
-    final = datetime.timedelta()
-    seconds = final.total_seconds()
+    seconds = datetime.timedelta().total_seconds()
     seconds = seconds*60
     words = p.number_to_words(seconds,andword=", ")
-    print(words,"minutes")
+    print(final,"minutes")
 
 class Datetime():
     def __init__(self,date,month,year):
         self.date=int(date)
         self.month=int(month)
         self.year=int(year)
+    def __str__(self):
+        return f"{self.date}-{self.month}-{self.year}"
 
     def __sub__(self,other):
         date = self.date-other.date
