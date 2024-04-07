f1b0f696c3f00e5ce5b73b0f94328056bed8dc0d
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 19:07:48 +0000

    Mon, Apr 8, 2024, 12:37 AM +05:30

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,7 +2,7 @@ import pytest
 from seasons import check
 
 def test_validdate():
-    assert check("2024-04-07")==("2024-04-07")
+    assert check("2024-04-07")=="2024-04-07"
 
 def test_invaliddate():
     with pytest.raises(ValueError):
