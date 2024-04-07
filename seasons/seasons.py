1abfea17fb5a3fdce16b7f15e632e835b5cffed0
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-07 19:00:19 +0000

    Mon, Apr 8, 2024, 12:30 AM +05:30

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,6 +1,16 @@
 import datetime,sys,inflect,re
 
 def main():
+
+    p=inflect.engine()
+    dob=Birthdate(birth_date)
+    date_of_birth=dob.check(birth_date)
+    today=datetime.date.today()
+    total_minutes=convert(date_of_birth,today)
+    words=p.number_to_words(total_minutes,andword=", ").lstrip("minus ")
+    print(words,"minutes")
+
+def check():
     try:
         birth_date=input("Date of Birth:")
         if re.search("^[0-9][0-9][0-9][0-9]-([0][0-9]||[1][0-2])+-([0][0-9]||[0-3][0-9])+$",birth_date):
@@ -9,19 +19,8 @@ def main():
             raise ValueError
     except ValueError:
         sys.exit("Invalid Date")
-    p=inflect.engine()
-    dob=Birthdate(birth_date)
-    date_of_birth=dob.check(birth_date)
-    today=datetime.date.today()
-    total_minutes=convert(date_of_birth,today)
-    words=p.number_to_words(total_minutes,andword=", ").lstrip("minus ")
-    print(words,"minutes")
-class Birthdate:
-    def __init__(self,birth_date):
-        self.year,self.month,self.date=birth_date.split("-")
-    def check(self,birth_date):
-        date_=datetime.date(int(self.year),int(self.month),int(self.date))
-        return date_
+    date_=datetime.date(int(self.year),int(self.month),int(self.date))
+    return date_
 def convert(date_of_birth,today):
     print(type(today))
     final=today-date_of_birth
