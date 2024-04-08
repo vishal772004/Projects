40a714f90d653188e8774a8eadd8ba2aeacd35cc
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 16:55:32 +0000

    Mon, Apr 8, 2024, 10:25 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,19 +2,13 @@ from fpdf import FPDF
 
 
 class PDF(FPDF):
-    def header(self):
-        
+    def __init__(self):
+        pdf.
+        pdf.add_page()
+        pdf.image("shirtificate.png")
+        pdf.set_font("Times", size=12)
 
-    def footer(self):
-        # Position cursor at 1.5 cm from bottom:
-        self.set_y(-15)
-        # Setting font: helvetica italic 8
-        self.set_font("helvetica", "I", 8)
-        # Printing page number:
-        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
 
 pdf = PDF()
-pdf.add_page()
-pdf.image("shirtificate.png")
-pdf.set_font("Times", size=12)
+
 pdf.output("varying_format.pdf")
