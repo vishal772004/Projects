936ce140b588f80fd405958218deb4340edf2d4d
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 17:28:15 +0000

    Mon, Apr 8, 2024, 10:58 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,10 +4,10 @@ from fpdf import FPDF
 class PDF(FPDF):
     def __init__(self):
         pdf =FPDF(orientation="portrait", format="A4")
-        pdf.add_page()
+        pdf.add_page(format=(210,297))
         pdf.set_font("Times",style="B",size=25)
         pdf.cell(text="CS50 Shirtificate",center="True",new_x="LMARGIN", new_y="NEXT")
-        
+
         pdf.image("shirtificate.png",h=160,w=160)
         pdf.output("varying_format.pdf")
 
