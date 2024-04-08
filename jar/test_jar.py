56ac5d0a8716254e808677fe43edb54bfdf9ba3b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 13:55:07 +0000

    Mon, Apr 8, 2024, 7:25 PM +05:30

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -3,6 +3,7 @@ from jar import Jar
 
 def test_init():
     jar=Jar()
+    assert jar.init(12)==None
 
 
 def test_str():
@@ -12,11 +13,17 @@ def test_str():
     assert str(jar) == "🍪"
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    jar.withdraw(5)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_deposit():
-    jar=J
+    jar=Jar()
+    assert jar.deposit(1) == 1
+    assert jar.deposit(5) == 5
 
 
 def test_withdraw():
-    ...
+    jar=Jar()
+    assert jar.withdraw(1) == 1
+    assert jar.withdraw(5) == 5
