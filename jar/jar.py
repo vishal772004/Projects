8d00d1e65a19d081268f04ffd0663427e34e4e27
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 12:29:19 +0000

    Mon, Apr 8, 2024, 5:59 PM +05:30

---
+++ jar/jar.py
@@ -0,0 +1,25 @@
+class Jar:
+    def __init__(self, capacity=12):
+        ...
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
+def main():
+    ...
+if __name__=="__main__":
+    main()
