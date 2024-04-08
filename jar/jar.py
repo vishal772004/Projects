1c94ada96c8c08c9a12c978d99e9ccb413eaa3e3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 13:34:21 +0000

    Mon, Apr 8, 2024, 7:04 PM +05:30

--- jar/jar.py
+++ jar/jar.py
@@ -1,46 +1,31 @@
-
-
-
 class Jar:
     def __init__(self, capacity=12):
         self.capacity=capacity
-        self.size=0
 
     def __str__(self):
         i="🍪"
         return self.size*i
 
     def deposit(self, n):
-        self.add=self.size+n
-        if self.add>self.capacity:
-            raise ValueError
-        return self.add
+        self.size
 
     def withdraw(self, n):
-        self.sub=self.size-n
-        if n>self.size:
-            raise ValueError
-        return self.sub
+        ...
 
     @property
     def capacity(self):
-        return self._capacity
-
+        return self.capacity
     @capacity.setter
-    def capacity(self,capacity):
+    def capacity(self,capacity=12):
         self._capacity=capacity
-        if self._capacity<0:
-            raise ValueError
 
     @property
     def size(self):
-        self.size=self.size+self.add
         return self.size
+
     @size.setter
     def size(self,size):
         self._size=size
-        if self._size>self.capacity:
-            raise ValueError
 
 def main():
     jar=Jar()
