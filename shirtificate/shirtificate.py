1225b615c659a5908a3bf96998d773f0a4e1f8bf
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 14:40:12 +0000

    Mon, Apr 8, 2024, 8:10 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,11 +3,9 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf = FPDF(orientation="portrait", format="A4")
 pdf.set_font("Helvetica")
-for i in range(9):
-    if i == 6:
-        pdf.set_page_background('shirtificate.png')
-    pdf.add_page(format=(210 * (1 - i/10), 297 * (1 - i/10)))
-    pdf.cell(text=str(i))
+pdf.set_page_background('shirtificate.png')
+pdf.add_page(format=(210 * (1 - i/10), 297 * (1 - i/10)))
+pdf.cell(text=)
 pdf.set_page_background(None)
 pdf.add_page(same=True)
 pdf.cell(text="9")
