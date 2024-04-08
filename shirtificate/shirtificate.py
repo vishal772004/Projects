bff2a6d0ebf77c949d394ebb8c2af4973ae6d4a1
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 17:25:12 +0000

    Mon, Apr 8, 2024, 10:55 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,7 +7,7 @@ class PDF(FPDF):
         pdf.add_page()
         pdf.set_font("Times",style="B",size=25)
         pdf.cell(text="CS50 Shirtificate",center="True",new_x="LMARGIN", new_y="NEXT")
-        pdf.image("shirtificate.png")
+        pdf.image("shirtificate.png",h=145,w=105)
         pdf.output("varying_format.pdf")
 
 
