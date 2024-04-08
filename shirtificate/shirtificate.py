464b97e2842e487e694997f08e74e204cb0f22b8
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 14:33:05 +0000

    Mon, Apr 8, 2024, 8:03 PM +05:30

---
+++ shirtificate/shirtificate.py
@@ -0,0 +1,14 @@
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.set_font("Helvetica")
+pdf.set_page_background((252,212,255))
+for i in range(9):
+    if i == 6:
+        pdf.set_page_background('shirtificate.png')
+    pdf.add_page(format=(210 * (1 - i/10), 297 * (1 - i/10)))
+    pdf.cell(text=str(i))
+pdf.set_page_background(None)
+pdf.add_page(same=True)
+pdf.cell(text="9")
+pdf.output("varying_format.pdf")
