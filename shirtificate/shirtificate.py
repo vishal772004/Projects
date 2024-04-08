deb7aab936ada9b1c9bc62a268bd3fff2538519e
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-08 18:07:20 +0000

    Mon, Apr 8, 2024, 11:37 PM +05:30

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -11,7 +11,7 @@ class PDF(FPDF):
         self.pdf.ln(20)
         self.pdf.cell(8,50)
         self.pdf.set_font("Times",style="B",size=20)
-        self.pdf.set_text_color(0,0,0)
+        self.pdf.set_text_color(255,255,255)
         self.pdf.cell(text=self.name,align="c")
         self.pdf.image("shirtificate.png",h=240, w=175)
         self.pdf.output("varying_format.pdf")
