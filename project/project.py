110fc46d264d5a34f3e7ecc173b969805f8a0aa4
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-09 18:15:15 +0000

    Tue, Apr 9, 2024, 11:45 PM +05:30

--- project/project.py
+++ project/project.py
@@ -3,7 +3,7 @@ import csv
 def main():
     print("\tWelcome to Movie Mania \t")
     print("On what basis do you want to find your movie :")
-    print("1.Rating\n2.Genre\n3.Movieyear\n4.Star Cast\n5.Language\n6.Random Movie)
+    print("1.Rating\n2.Genre\n3.Movieyear\n4.Star Cast\n5.Language\n6.Random Movie")
     choice=int(input("Please Enter your choice:"))
     match choice:
         case 1: Rating()
@@ -18,7 +18,9 @@ def function_2():
 def function_n():
     ...
 def csv_Reader():
-    
+    with open("IMDBTop250.csv") as file:
+        
+
 
 if __name__ == "__main__":
     main()
