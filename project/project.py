444bc86151a350f132445b2fc03696a78d72070c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-09 18:01:21 +0000

    Tue, Apr 9, 2024, 11:31 PM +05:30

--- project/project.py
+++ project/project.py
@@ -1,15 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
 
 def main():
-    response = requests.get('http://www.imdb.com/chart/top')
-    soup = BeautifulSoup(response.text, "html.parser")
-    movies = soup.select('td.titleColumn')
-    crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-    ratings = [b.attrs.get('data-value')
-        for b in soup.select('td.posterColumn span[name=ir]')]
-    print(movies)
-    https://github.com/ExplorerMunchkin/web-scraping-imdb-top250-python/raw/main/IMDBTop250.csv
+    print("\tWelcome to Movie Mania \t")
+    print(")
 def function_1():
     ...
 
