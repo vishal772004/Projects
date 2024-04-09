17341887153715f7fe6901c14a79ad780ad56533
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-09 18:11:10 +0000

    Tue, Apr 9, 2024, 11:41 PM +05:30

--- project/project.py
+++ project/project.py
@@ -3,9 +3,10 @@ def main():
     print("\tWelcome to Movie Mania \t")
     print("On what basis do you want to find your movie :")
     print("1.Rating\n2.Genre\n3.Movieyear\n4.Star Cast\n5.Language\n6.Random Movie)
-    choice=input("Please Enter your choice:")
-    match 
-def function_1():
+    choice=int(input("Please Enter your choice:"))
+    match choice:
+        case 1: Rating()
+def Rating():
     ...
 
 
