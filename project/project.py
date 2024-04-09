6303fb12e0b1d6269735d0c77224fabc38c3bd54
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-09 17:36:22 +0000

    Tue, Apr 9, 2024, 11:06 PM +05:30

--- project/project.py
+++ project/project.py
@@ -2,9 +2,9 @@ import requests
 
 
 def main():
-    response = requests.get("")
-
-
+    response = requests.get("https://aax-eu.amazon-adsystem.com/x/c/RMr0Vz6yP3TH_6FyXihShw4AAAGOw-0CYgMAAADKAQBvbm9fdHhuX2JpZDEgICBvbm9fdHhuX2ltcDEgICBAAk9U/https://www.primevideo.com/detail/amzn1.dv.gti.4b0528be-682a-47fa-8016-f3002f05bfc2/?ref_=dvm_crs_imd_in_s_teribaaton")
+    reader = response.json()
+    print(reader)
 def function_1():
     ...
 
