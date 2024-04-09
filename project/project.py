b277c993822484d24b1f5927df9a5a3907dbbded
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-09 18:21:55 +0000

    Tue, Apr 9, 2024, 11:51 PM +05:30

--- project/project.py
+++ project/project.py
@@ -5,8 +5,7 @@ def main():
     print("On what basis do you want to find your movie :")
     print("1.Rating\n2.Genre\n3.Movieyear\n4.Star Cast\n5.Language\n6.Random Movie")
     choice=int(input("Please Enter your choice:"))
-    match choice:
-        case 1: Rating()
+    csv_Reader()
 def Rating():
     ...
 
@@ -20,7 +19,9 @@ def function_n():
 def csv_Reader():
     with open("IMDBTop250.csv") as file:
         reader = csv.DictReader(file)
-        
+        rating = {row for row in reader if rating>9}
+        print(rating)
+
 
 
 if __name__ == "__main__":
