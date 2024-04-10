ac5056760b15e6bc57446e9ca369940c26021b4a
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-10 07:06:11 +0000

    Wed, Apr 10, 2024, 12:36 PM +05:30

--- project/project.py
+++ project/project.py
@@ -10,7 +10,7 @@ def main():
 
 def Rating():
     row = csv_Reader()
-    rating = [ r for r in row if r['rating']]
+    rating = [ r['rating'] for r in row ]
     print(rating)
 
 def function_2():
