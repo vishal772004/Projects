43f64de4719dc69fccd90b78b9c78a6da1ac95df
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-04-10 11:15:45 +0000

    Wed, Apr 10, 2024, 4:45 PM +05:30

--- project/test_project.py
+++ project/test_project.py
@@ -1,3 +1,23 @@
 import pytest
+from project import option1
 
-def test_
+def test_option1():
+    assert option1(1,"Action,Adventure") == True
+    assert option1(2,"Action,Adventure,Drama") == True
+    assert option1(3,"Drama,Crime") == True
+    assert option1(4,"Action,Crime") == True
+    assert option1(5,"History,Crime") == True
+    assert option1(6,"Comedy") == True
+    assert option1(7,"Fantasy") == True
+    assert option1(8,"Animation") == True
+
+
+def test_invalid_option1():
+    assert option1(1,"Adventure,Drama") == None
+    assert option1(2,"Action,Drama") == None
+    assert option1(3,"Crime") == None
+    assert option1(4,"Action") == None
+    assert option1(5,"Crime") == None
+    assert option1(6,"Action") == None
+    assert option1(7,"Adventure") == None
+    assert option1(8,"Fantasy") == None
