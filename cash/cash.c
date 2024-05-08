c08c9f38dd2b11b2a5d09bc3d66672e37e511363
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-08 07:06:42 +0000

    Wed, May 8, 2024, 12:36 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -10,6 +10,26 @@ int main()
     while(cash<0);
     while(cash!=0)
     {
-        if()
+       if(cash>25)
+       {
+        cash-=25;
+        coins++;
+       }
+       else if(cash>10)
+       {
+        cash-=10;
+        coins++;
+       }
+       else if(cash>5)
+       {
+        cash-=5;
+        coins++;
+       }
+       else
+       {
+        cash-=1;
+        coins++;
+       }
     }
+    printf(coins);
 }
