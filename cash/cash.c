122332790c556a373be7d2790fff882e360e947c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-08 07:03:17 +0000

    Wed, May 8, 2024, 12:33 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -2,11 +2,14 @@
 #include<cs50.h>
 int main()
 {
-    int cash;
+    int cash,coins=0;
     do
     {
         cash = get_int("Change owed?");
     }
     while(cash<0);
-    
+    while(cash!=0)
+    {
+        if()
+    }
 }
