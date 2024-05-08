4463051339b5391695bdf88e4e2041742300ef9e
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-08 06:31:47 +0000

    Wed, May 8, 2024, 12:01 PM +05:30

---
+++ mario/mario.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<cs50.h>
+int main()
+{
+    int height;
+    do
+    {
+        int height = get_int("Height:");
+    }while(height>0);
+
+    for (int i=0;i<height;i++)
+    {
+        for (int j=0;j<height;j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
