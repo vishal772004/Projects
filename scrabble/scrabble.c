0533e71318e1de51e5b313eb982aefb99eb49867
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 11:48:05 +0000

    Sun, May 12, 2024, 5:18 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,5 +5,6 @@ int main(void)
 {
     string player_1 = get_string("Player 1:");
     string player_2 = get_string("Player 2:");
+    int points ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     
 }
