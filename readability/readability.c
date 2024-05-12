ec6646ee6c8b4a8b7c34abe0e23923fa23bc3c30
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 12:23:26 +0000

    Sun, May 12, 2024, 5:53 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 
 int main(void)
 {
+    int no_of_letters=0;
     string text =get_string("Text:");
-    
+    for (int i=0,n=strlen(text);i<n;i++)
+    {
+        if(text[i]!=" ")
+        {
+            no_of_letters++;
+        }
+    }
+    printf("%d",no_of_letters);
 }
