27973185ca47a0c0aaedff71e76d1f077faf254c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 12:53:30 +0000

    Sun, May 12, 2024, 6:23 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -19,7 +19,15 @@ int main(void)
     index = 0.0588*L - 0.296*S -15.8;
     if (index>=16)
     {
-        printf(")
+        printf("Grade 16+");
+    }
+    else if(index<=1)
+    {
+        printf("Grade 1");
+    }
+    else
+    {
+        printf("Grade %d",)
     }
 }
 int no_of_letters(string s)
