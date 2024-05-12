ca74c7ae724bffd7690ffcceb531d2f164ed8882
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 12:44:15 +0000

    Sun, May 12, 2024, 6:14 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -2,19 +2,47 @@
 #include<cs50.h>
 #include<string.h>
 
+int no_of_letters(string s);
+int no_of_words(string s);
+int no_of_sentences(string s)
+
 int main(void)
 {
-    int no_of_letters=0,no_of_words=0,no_of_sentences=0;
+    int letters,words,sentences;
     string text =get_string("Text:");
+    letters = no_of_letters(text);
+    words = no_of_words(text);
+    sentences = no_of_sentences(text);
+
+
+
+}
+int no_of_letters(string s)
+{
+    int count = 0;
+     for (int i=0,n=strlen(text);i<n;i++)
+    {
+        count++;
+    }
+}
+int no_of_words(string s)
+{
+    int count =0;
     for (int i=0,n=strlen(text);i<n;i++)
     {
         if (text[i]==' '||text[i]=='.')
-            no_of_words++;
-        if (text[i]=='.')
-            no_of_sentences++;
-        no_of_letters++;
+            count++;
     }
-    printf("%d\n",no_of_letters);
-    printf("%d\n",no_of_words);
-    printf("%d\n",no_of_sentences);
+    return count;
+}
+
+int no_of_sentences(string s)
+{
+    int count=0;
+    for (int i=0,n=strlen(text);i<n;i++)
+    {
+        if (text[i]=='.')
+            count++;
+    }
+    return count;
 }
