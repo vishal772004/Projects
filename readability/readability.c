c0c14b446afbf2b8a95a21fa3ff4f8d939845c21
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 12:33:43 +0000

    Sun, May 12, 2024, 6:03 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -4,12 +4,14 @@
 
 int main(void)
 {
-    int no_of_letters=0;
+    int no_of_letters=0,no_of_words=0,no_of_sentences=0;
     string text =get_string("Text:");
     for (int i=0,n=strlen(text);i<n;i++)
     {
         if (text[i]==' ')
-            continue;
+            no_of_words++;
+        if (text[i]=='.')
+            no_of_sentences++;
         no_of_letters++;
     }
     printf("%d\n",no_of_letters);
