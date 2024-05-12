1124290a3bd361fbe3d72bff7638e5edc9a4a762
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-12 12:02:43 +0000

    Sun, May 12, 2024, 5:32 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -9,16 +9,31 @@ int main(void)
 {
     string player_1 = get_string("Player 1:");
     string player_2 = get_string("Player 2:");
-    int points ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
+    int score1 = score(player_1);
+    int score2 = score(player_2);
+    if (score1>score2)
+    {
+        printf("Player 1 Wins!");
+    }
+    else if(score2>score1)
+    {
+        printf("Player 2 Wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
 
 int score(string s)
 {
+    int points[] ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     int scoreboard = 0;
     for (int i=0,n=strlen(s);i<n;i++)
     {
-        int value = toupper(s[i]);
-        
+        int value = toupper(s[i])-65;
+        scoreboard +=points[value];
     }
+    return scoreboard;
 }
