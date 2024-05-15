72dd4159297e50b7a54f62edce49ec598bc9dfa9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-15 14:05:36 +0000

    Wed, May 15, 2024, 7:35 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -31,7 +31,8 @@ void cipher(string s,int k)
         temp=s[i]+k;
             if(islower(s[i]) && temp>122)
             {
-                temp = (temp - 122) +
+                while()
+                temp = (temp - 122) + 97;
             }
         printf("%c",);
     }
