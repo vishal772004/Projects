08fca1df235b70c6e5a16a30222d4db641793029
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-15 13:33:51 +0000

    Wed, May 15, 2024, 7:03 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -11,7 +11,7 @@ int main(int argc,string argv[])
     if(argc==2)
     {
             string plaintext = get_string("plaintext:");
-            int k = int(argv[]);
+            int k = int(argv[1]);
             string ans = cipher(plaintext,k);
             printf("%s",ans);
             return 0;
