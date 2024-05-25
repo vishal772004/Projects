90480964dce9fc2753514c2b19f969085718a5f1
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-25 12:11:08 +0000

    Sat, May 25, 2024, 5:41 PM +05:30

--- plurality/plurality/plurality.c
+++ plurality/plurality/plurality.c
@@ -80,8 +80,8 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    string winner;
-    int winning_votes = candidates[0].votes;
+    string winner[];
+    int winning_votes[] = {candidates[0].votes};
     for (int i=0;i < candidate_count;i++)
     {
         if (winning_votes<candidates[i].votes)
