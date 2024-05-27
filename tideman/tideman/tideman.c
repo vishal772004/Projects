f68790ed98dd29f3c707a7d2bd3db14d798dda3c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-27 15:18:22 +0000

    Mon, May 27, 2024, 8:48 PM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -98,7 +98,10 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    for (int i=0;i<candidate_count;i++)
+    {
+        if(strcmp(name,candidates))
+    }
     return false;
 }
 
