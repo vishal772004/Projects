b7f551d5ae469e5d54ff73ecdd3397d5489de419
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-28 14:33:32 +0000

    Tue, May 28, 2024, 8:03 PM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -133,7 +133,10 @@ void add_pairs(void)
     // TODO
     for (int i=0;i<candidate_count;i++)
     {
-        
+        for (int j=0;j<candidate_count;j++)
+        {
+            
+        }
     }
     return;
 }
