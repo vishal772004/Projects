c3adc0d9023a341388d58bfa2ed499f9be49896d
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-28 14:37:19 +0000

    Tue, May 28, 2024, 8:07 PM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -131,6 +131,7 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    pair_count=0;
     for (int i=0;i<candidate_count;i++)
     {
         for (int j=0;j<candidate_count;j++)
