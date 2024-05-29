32f90e2bfe72c95ebfddb714520d9a7176b78ffb
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-05-29 14:56:00 +0000

    Wed, May 29, 2024, 8:26 PM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -197,11 +197,15 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
+    int source=0;
     for(int i=0;i<candidate_count;i++)
     {
         for(int j=0;j<candidate_count;j++)
         {
-            
+            if(locked[i][j]==true)
+            {
+                source = i;
+            }
         }
     }
 
