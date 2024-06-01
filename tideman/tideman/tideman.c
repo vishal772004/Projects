09bea065099d9d18804d2c495d71497cbac52e83
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-06-01 20:18:47 +0000

    Sun, Jun 2, 2024, 1:48 AM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -121,7 +121,7 @@ void record_preferences(int ranks[])
         {
             if(i==j)
                 preferences[i][j]=0;
-            else if(i<j)
+            else if(ranks[i]<ranks[j])
             {
                 preferences[i][j]+=1;
 
