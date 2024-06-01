c16bcb6b83e64e518031ff5f1422ae928fb640b6
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-06-01 09:57:37 +0000

    Sat, Jun 1, 2024, 3:27 PM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -113,7 +113,16 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-
+    for (int i=0;i<candidate_count;i++)
+    {
+        for(int j=0;j<candidate_count;j++)
+        {
+            if(i!=j)
+            {
+                preferences[i][j]=
+            }
+        }
+    }
 }
 
 // Record pairs of candidates where one is preferred over the other
