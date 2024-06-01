c309a9189d93f3b36e745a77d8e2df63d451f173
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-06-01 18:43:05 +0000

    Sun, Jun 2, 2024, 12:13 AM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -183,6 +183,13 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
+    for(int i=0;i<candidate_count;i++)
+    {
+        for(int j=0;j<candidate_count;j++)
+        {
+            if()
+        }
+    }
 
 }
 
