5dabb61d9a95a9b231d4f42e42396f021593dabf
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-06-01 18:45:26 +0000

    Sun, Jun 2, 2024, 12:15 AM +05:30

--- tideman/tideman/tideman.c
+++ tideman/tideman/tideman.c
@@ -183,11 +183,11 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
-    for(int i=0;i<candidate_count;i++)
+    for(int i=0;i<pair_count;i++)
     {
-        for(int j=0;j<candidate_count;j++)
+        if(preferences[pairs[i].winner][pairs[i].loser]!=0)
         {
-            if()
+            
         }
     }
 
