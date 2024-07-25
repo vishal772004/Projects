3b37548287a739e4846dfe91e560ca7c82fc4a2f
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-25 10:19:47 +0000

    Thu, Jul 25, 2024, 3:49 PM +05:30

--- filter-less/filter-less/helpers.c
+++ filter-less/filter-less/helpers.c
@@ -47,6 +47,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i=0;i<height;i++)
+    {
+        for(int j=0;i<width;j++)
+        {
+            
+        }
+    }
     return;
 }
 
