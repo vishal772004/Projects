cfbbdf1a59c65961804d465d0864be6224ea1ed0
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-25 18:43:33 +0000

    Fri, Jul 26, 2024, 12:13 AM +05:30

--- recover/recover/recover.c
+++ recover/recover/recover.c
@@ -18,7 +18,10 @@ int main(int argc, char *argv[])
 
     while (fread(&buffer, 1, 512, card) == 512)
     {
-            
+            if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0)==0xe0)
+            {
+                
+            }
     }
     fclose(card);
 }
