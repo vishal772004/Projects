a1f847cde8db05bf2f0b51542e27cc6c8559904b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-25 18:48:08 +0000

    Fri, Jul 26, 2024, 12:18 AM +05:30

--- recover/recover/recover.c
+++ recover/recover/recover.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     {
             if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0)==0xe0)
             {
-                
+                sprintf("000.jpg","%03i.jpg",)
             }
     }
     fclose(card);
