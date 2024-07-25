109b489fc9d020ca1250c4c480ab9ac39b31cf6b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-25 19:09:21 +0000

    Fri, Jul 26, 2024, 12:39 AM +05:30

--- recover/recover/recover.c
+++ recover/recover/recover.c
@@ -16,13 +16,15 @@ int main(int argc, char *argv[])
     }
     uint8_t buffer[512];
     int n=0;
+    FILE *img = NULL;
+
     while (fread(&buffer, 1, 512, card) == 512)
     {
             if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0)==0xe0)
             {
                 sprintf(buffer,"%03i.jpg",n);
                 n++;
-                FILE *img = fopen(buffer,"w");
+
                 if(n==0)
                 {
                     fwrite()
