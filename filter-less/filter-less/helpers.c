9913982cc48e8c4774f9732f018ae8623bf0799d
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-25 10:51:10 +0000

    Thu, Jul 25, 2024, 4:21 PM +05:30

--- filter-less/filter-less/helpers.c
+++ filter-less/filter-less/helpers.c
@@ -63,5 +63,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+
+    
     return;
 }
