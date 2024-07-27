c0e0ab4e31732452898dfd880add94e5815b6d60
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-27 18:12:05 +0000

    Sat, Jul 27, 2024, 11:42 PM +05:30

--- inheritance/inheritance/inheritance.c
+++ inheritance/inheritance/inheritance.c
@@ -49,6 +49,7 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        new_person->parent[0] = parent0;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
