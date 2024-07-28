7ab6a3927bf81dcdf616940f8a92b177e62ac5b9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-28 12:08:24 +0000

    Sun, Jul 28, 2024, 5:38 PM +05:30

--- speller/speller/dictionary.c
+++ speller/speller/dictionary.c
@@ -76,7 +76,7 @@ bool load(const char *dictionary)
         size_of++;
     }
     fclose(source);
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
