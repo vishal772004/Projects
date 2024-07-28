c7254301a70cd26dbb1278e8668ce8c31a901b7c
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-28 12:54:30 +0000

    Sun, Jul 28, 2024, 6:24 PM +05:30

--- speller/speller/dictionary.c
+++ speller/speller/dictionary.c
@@ -16,7 +16,7 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 52;
+const unsigned int N = 100;
 unsigned int size_of = 0;
 // Hash table
 node *table[N];
@@ -41,7 +41,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return strlen(word);
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
