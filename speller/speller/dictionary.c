74a951ae1db05c04587ea2d9af11a9942b6fc3e3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-28 11:56:17 +0000

    Sun, Jul 28, 2024, 5:26 PM +05:30

--- speller/speller/dictionary.c
+++ speller/speller/dictionary.c
@@ -54,7 +54,7 @@ bool load(const char *dictionary)
     {
         return false;
     }
-    char *word = NULL;
+    char word[LENGTH+1];
     while(fscanf(source,"%s",word)!=EOF)
     {
         node *n = malloc(sizeof(node));
