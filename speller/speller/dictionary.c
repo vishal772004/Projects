5cc05ce299ae85567f2643ded200342efc0c911b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-28 12:53:03 +0000

    Sun, Jul 28, 2024, 6:23 PM +05:30

--- speller/speller/dictionary.c
+++ speller/speller/dictionary.c
@@ -16,7 +16,7 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 52;
 unsigned int size_of = 0;
 // Hash table
 node *table[N];
