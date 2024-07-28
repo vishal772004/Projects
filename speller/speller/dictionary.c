aabb42c0e40c3521cc16208481c461e962bdb019
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-07-28 12:51:04 +0000

    Sun, Jul 28, 2024, 6:21 PM +05:30

--- speller/speller/dictionary.c
+++ speller/speller/dictionary.c
@@ -41,7 +41,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    return strlen(word);
 }
 
 // Loads dictionary into memory, returning true if successful, else false
