e830371d287fdd522c822761ea12f668d206c1b4
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-05 14:36:33 +0000

    Mon, Aug 5, 2024, 8:06 PM +05:30

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,9 @@
 height = int(input("Height:"))
 for i in range(height):
+    for k in range(height-i):
+        print(" ",end="")
+    for l in range(i+1):
+        print("#",end="")
     for j in range(i+1):
         print("#",end="")
     print()
