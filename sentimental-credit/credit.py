60764322d8dd225875c1def8c27d9bd243e4ed6b
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-05 15:41:21 +0000

    Mon, Aug 5, 2024, 9:11 PM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,21 +1,27 @@
-import re
+import re,sys
 num = input("Number:")
 prod = 1
 sum = 0
 l = []
 for i in num:
     l.append(int(i))
+
+if(re.search("^(34)|(37)[0-9]{13}",num)):
+    print("AMEX")
+    sys.exit(0)
+
+
+if(re.search("^5+[1-5]{13}",num)):
+    print("AMEX")
+    sys.exit(0)
+
 for i in range(len(l)):
     if i%2==0 or i==0:
         prod = prod + (l[i]*2)
     else:
         sum = sum + l[i]
-maxsum = prod+sum
-print(maxsum)
+
 if((prod+sum)%10==0):
     pass
 else:
     print("INVALID")
-
-if(re.search("^(34)|(37)[0-9]{13}",num)):
-    print("AMEX")
