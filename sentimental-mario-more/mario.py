e0ef60ce4e8df22667d87506965a074e085b2ae9
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-05 14:51:58 +0000

    Mon, Aug 5, 2024, 8:21 PM +05:30

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,11 +2,12 @@ import sys
 
 while(True):
     height = input("Height:")
-    if(height.isdigit() and int(height>0)):
+    if(height.isdigit() and int(height)>0):
         break
+height = int(height)
 
 for i in range(height):
-    for k in range(height-i):
+    for k in range(height-i-1):
         print(" ",end="")
     for l in range(i+1):
         print("#",end="")
