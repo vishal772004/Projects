11c54b508c2a288b9ee5e05ab22cca2b158f19df
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-05 14:37:50 +0000

    Mon, Aug 5, 2024, 8:07 PM +05:30

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,6 +4,7 @@ for i in range(height):
         print(" ",end="")
     for l in range(i+1):
         print("#",end="")
+    print("  ",end="")
     for j in range(i+1):
         print("#",end="")
     print()
