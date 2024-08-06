60738b934cce08fcc1e3d949b32c2ef6c5bcd6a5
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-06 17:43:26 +0000

    Tue, Aug 6, 2024, 11:13 PM +05:30

--- dna/dna/dna.py
+++ dna/dna/dna.py
@@ -9,6 +9,7 @@ def main():
         sys.exit(1)
 
     # TODO: Read database file into a variable
+    
     with open(sys.argv[1]) as file:
         dreader = csv.DictReader(file)
     print(dreader.fieldnames)
