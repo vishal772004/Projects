2a38698eac566a711d83f5afa1b95d007e6f1572
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-06 18:12:45 +0000

    Tue, Aug 6, 2024, 11:42 PM +05:30

--- dna/dna/dna.py
+++ dna/dna/dna.py
@@ -22,7 +22,8 @@ def main():
         l2.append(sreader.fieldnames)
     print(l2)
     # TODO: Find longest match of each STR in DNA sequence
-    #longest_run = longest_match()
+    for i in 
+    longest_run = longest_match()
     # TODO: Check database for matching profiles
 
     return
