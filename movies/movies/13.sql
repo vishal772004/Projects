ab030b454f45bc7c1c65bdefa370f32b666c4fb4
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-11 19:04:48 +0000

    Mon, Aug 12, 2024, 12:34 AM +05:30

--- movies/movies/13.sql
+++ movies/movies/13.sql
@@ -1,2 +1,2 @@
 SELECT p.name FROM movies AS m JOIN stars AS s ON m.id=s.movie_id JOIN people AS p ON p.id=s.person_id WHERE NOT p.name="Kevin Bacon" AND s.movie_id IN
-(SELECT p.name FROM movies AS m JOIN stars AS s ON m.id=s.movie_id JOIN people AS p ON p.id=s.person_id WHERE p.name="Kevin Bacon" AND p.birth=1958);
+(SELECT movie_id FROM movies AS m JOIN stars AS s ON m.id=s.movie_id JOIN people AS p ON p.id=s.person_id WHERE p.name="Kevin Bacon" AND p.birth=1958);
