aaf86d776d91e4af51b6a53330d0ee6d072ee077
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-11 15:39:48 +0000

    Sun, Aug 11, 2024, 9:09 PM +05:30

---
+++ movies/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT m.title FROM movies AS m JOIN ratings AS r ON m.id=r.movie_id JOIN stars AS s ON m.id=s.movie_id JOIN people AS p ON s.person_id=p.id WHERE p.name ="Chadwick Boseman"ORDER BY r.rating DESC LIMIT 5;
