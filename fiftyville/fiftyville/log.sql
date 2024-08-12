845292ed3b767733eb6d033def2f7766e88633f3
vishal772004 <158483863+vishal772004@users.noreply.github.com>
2024-08-12 11:32:26 +0000

    Mon, Aug 12, 2024, 5:02 PM +05:30

--- fiftyville/fiftyville/log.sql
+++ fiftyville/fiftyville/log.sql
@@ -1,2 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+/* Starting from the crime scene reports table */
+SELECT *
+FROM crime_scene_reports
+WHERE day=28
+AND month=7
+AND year=2023
+AND street="Humphrey Street";
